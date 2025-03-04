257f32dceaba2dd9440dbb7f74d752d674b17482
daVasst Ngô hi en <vanmanhn868@gmail.com>
2025-03-04 17:25:53 +0700

    haizz

--- BTLNhapMonCNPM/obj/Debug/net8.0/BTLNhapMonCNPM.AssemblyInfo.cs
+++ BTLNhapMonCNPM/obj/Debug/net8.0/BTLNhapMonCNPM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLNhapMonCNPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ba38f9b376085c899478d287053e7c46ef7dc83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673572125fcf643683fa4cccac255f4ac42c9d2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLNhapMonCNPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLNhapMonCNPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
